7ec7e5c0bfc065c822b497a9e0c0f2815cafcf68
vngldkh <idgladkikh@edu.hse.ru>
2021-09-13 20:12:18 +0300

    2

---
+++ Module_1/Seminar_2/HW/Tak_07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tak_07
+{
+    class Program
+    {
+        public static string F(double x) // Возвращает целую и дробную части числа в виде строки
+        {
+            return String.Format("Целая часть: {0}. Дробная часть: {1}.", Math.Floor(x), x - Math.Floor(x));
+        }
+        public static string G(double x) // Возвращает корень из числа и его квадрат в виде строки
+        {
+            string res;
+            if (x >= 0)
+                res = String.Format("Корень: {0}. Квадрат: {1}.", Math.Sqrt(x), x * x);
+            else
+                res = String.Format("Корень: i * {0}. Квадрат {1}.", Math.Sqrt(Math.Abs(x)), x * x);
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Введите вещественное число: ");
+                string input = Console.ReadLine();
+                double x;
+                if (double.TryParse(input, out x))
+                    Console.WriteLine(F(x) + " " + G(x));
+                else
+                    Console.WriteLine("Ошибка ввода!");
+                Console.WriteLine("Для выхода нажмите ESC. Для продолжения - любую другую клавишу.");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Module_1/Seminar_2/HW/Task_06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+do
+{
+    Console.Write("Бюджет: ");
+    string sa = Console.ReadLine();
+    Console.Write("Процент: ");
+    string sp = Console.ReadLine();
+
+    decimal a;
+    int p;
+
+    if (decimal.TryParse(sa, out a) && int.TryParse(sp, out p))
+    {
+        a = a * p / 100;
+        Console.WriteLine($"Сумма, выделенная на компьютерные игры: {a:C2}");
+    }
+    else
+        Console.WriteLine("Oшибка ввода!");
+    Console.WriteLine("Для выхода нажмите ESC. Для продолжения - любую другую клавишу.");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
