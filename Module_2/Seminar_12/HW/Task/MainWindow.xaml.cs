c9d08b2832388b657604f1c130c36e828ed7aac3
vngldkh <idgladkikh@edu.hse.ru>
2021-12-17 18:38:01 +0300

    HW12

---
+++ Module_2/Seminar_12/HW/Task/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            polyline.Points.Add(new Point(sliderW.Value + 10, 195 - sliderV.Value));
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            polyline.Points.Clear();
+        }
+    }
+}
