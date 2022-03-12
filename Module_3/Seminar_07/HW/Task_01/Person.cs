9d1d38561356c44bf8962cee7d76091618ca52a3
vngldkh <idgladkikh@edu.hse.ru>
2022-03-13 00:02:32 +0300

    HW7

---
+++ Module_3/Seminar_07/HW/Task_01/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_01
+{
+    public struct Person : IComparable
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public int Age { get; }
+
+        public Person(string name, string lastName, int age)
+        {
+            if (age < 0)
+                throw new ArgumentOutOfRangeException();
+            (Name, LastName, Age) = (name, lastName, age);
+        }
+
+        public int CompareTo(object obj)
+            => Age.CompareTo(((Person) obj).Age);
+
+        public override string ToString()
+            => $"Name : {Name} | Last name : {LastName} | Age : {Age}";
+    }
+}
---
+++ Module_3/Seminar_07/HW/Task_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var arr = new Person[rnd.Next(3, 10)];
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = new Person(GenerateString(), GenerateString(), rnd.Next(1, 101));
+            Array.ForEach(arr, x => Console.WriteLine(x));
+            Console.WriteLine("\n* * * * *\n");
+            Array.Sort(arr);
+            Array.ForEach(arr, x => Console.WriteLine(x));
+        }
+
+        private static string GenerateString()
+        {
+            var rnd = new Random();
+            var s = new StringBuilder();
+            s.Append((char) rnd.Next('A', 'Z' + 1));
+            var n = rnd.Next(0, 7);
+            for (var i = 0; i < n; i++)
+                s.Append((char) rnd.Next('a', 'z' + 1));
+            return s.ToString();
+        }
+    }
+}
