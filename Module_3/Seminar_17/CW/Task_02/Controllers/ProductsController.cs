76d1e0f9510ebc4ee2422c2e57047a36b7e9bfb5
vngldkh <idgladkikh@edu.hse.ru>
2022-03-24 09:50:09 +0300

    CW17

--- Module_3/Seminar_17/CW/Task_02/Controllers/ProductsController.cs
+++ Module_3/Seminar_17/CW/Task_02/Controllers/ProductsController.cs
@@ -4,31 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Task_02.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
     public class ProductsController
     {
-        private static List<Product> list = new List<Product>()
+        private static List<Product> list = new List<Product>(new[]
         {
-            new Product() { Id = 1,  Name = "fdhg", Price = 192 },
-            new Product() { Id = 2,  Name = "fask", Price = 24 },
-            new Product() { Id = 3,  Name = "dfgr", Price = 535 },
-        };
+            new Product() {Id = 1, Name = "gergr", Price = 12},
+            new Product() {Id = 2, Name = "ergre", Price = 145},
+            new Product() {Id = 3, Name = "regrae", Price = 4325},
+        });
 
         [HttpGet]
         public IEnumerable<Product> Get() => list;
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public IActionResult Get(int id)
         {
             var product = list.SingleOrDefault(p => p.Id == id);
-
             if (product == null)
-                return NotFoundResult();
-
+                return new NotFoundResult();
             return new OkObjectResult(product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            list.Remove(list.SingleOrDefault(p => p.Id == id));
+            return new OkResult();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            product.Id = 123;
+            list.Add(product);
+            return new CreatedResult(nameof(Get), product);
         }
     }
 }
